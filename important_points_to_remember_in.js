5a3d5e8ebfa38d9cb87e07059ac76a0d4886eb4e
kanishkakalra <162669030+kanishkakalra@users.noreply.github.com>
2024-05-02 21:33:40 +0000

    datatypes

---
+++ important_points_to_remember_in.js
@@ -0,0 +1,9 @@
+// for printing to many console we can use **console.table**console.table([accountEmail,accountId,accountcity,password]); 
+// we use "use strict" to tell the browser that we will use the new version of js
+// null is a standalone value
+// symbol is very unique
+//console.log(typeof xyz);
+//undefined is a type
+//null is an object
+
+//objects
---
+++ jspractice/basics.js
@@ -0,0 +1,8 @@
+console.log('hi');
+const accountId = 112234;
+let accountEmail = "kanishkakalra5152@google.com";
+var password= "1234"
+accountcity = "jaipur"
+
+// accountId = 2;
+console.table([accountEmail,accountId,accountcity,password]);
